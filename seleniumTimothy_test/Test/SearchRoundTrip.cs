09632285f3cc4eda14b348ccf64adeac56f66b70
tsmtih145 <timothesmith0320@gmail.com>
2025-06-17 14:08:16 -0400

    switchto

--- seleniumTimothy_test/Test/SearchRoundTrip.cs
+++ seleniumTimothy_test/Test/SearchRoundTrip.cs
@@ -6,25 +6,25 @@ using OpenQA.Selenium.Support.UI;
 
 public class SearchRoundTrip
 {
-    [Test, Order(1)]
+    [Test, Order(0)]
     [Category("Selenium")]
     public void OpenBrowser()
     {
 
-        var expectedAlertText = "Change Your Password";
+        
+
         IWebDriver driver = new ChromeDriver(@"C:\Users\Mjrlo\Downloads\chromedriver-win64 (2)\csharpdriver");
-        driver.Navigate().GoToUrl("https://practice.expandtesting.com/login/");
+        driver.Navigate().GoToUrl("https://www.delta.com/");
 
 
         DeltaLandingPage deltaLanding = new DeltaLandingPage(driver);
-        loginTest.enterPassword();
-        loginTest.enterUsername();
+        
 
 
-        loginTest.clickButton();
-        var alert = driver.SwitchTo().Alert();
-        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+        
+     //   var alert = driver.SwitchTo().Alert();
+      //  var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
 
-        Assert.Equals(expectedAlertText, alert.Text);
+///Assert.Equals(expectedAlertText, alert.Text);
     }
 }  
--- seleniumTimothy_test/page/DeltaLandingPage.cs
+++ seleniumTimothy_test/page/DeltaLandingPage.cs
@@ -7,13 +7,31 @@ public class DeltaLandingPage
 
     IWebDriver _driver;
 
-    //[FindsBy(How = How.Id, Using = "username")]
-    IWebElement _usernameTextBox => _driver.FindElement(By.Id("username"));
+    ////div[@class='airport-lookup-tablist-container pl-0 pr-0 mt-2']
+   
 
     ////span[normalize-space()='To']
     IWebElement _arrivalCityButton => _driver.FindElement(By.XPath("//span[normalize-space()='To']"));
 
     IWebElement _departureCityButton => _driver.FindElement(By.XPath("//span[normalize-space()='ATL']"));
-    
+
+    IWebElement _destinationAlertBox => _driver.FindElement(By.XPath("//div[@class='airport-lookup-tablist-container pl-0 pr-0 mt-2']"));
+
+     ////input[@id='search_input']
+    public DeltaLandingPage(IWebDriver driver)
+    {
+        this._driver = driver;
+
+
+    }
+
+
+    public void ChooseDestination()
+    {
+        _arrivalCityButton.Click();
+
+        _driver.SwitchTo().Frame(_destinationAlertBox);
+        
+    }
 }
      
